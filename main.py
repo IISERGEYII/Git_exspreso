70b6954bd3e2212f87d4fcec490f2962876c1831
II <s.kazarnikov@gmail.com>
2024-12-31 17:44:58 +0300

    Капучино

--- main.py
+++ main.py
@@ -1,52 +1,69 @@
+import sqlite3
 import sys
 
-from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt6.QtWidgets import QWidget, QTableView, QApplication
+from PyQt6.QtWidgets import QWidget, QTableWidgetItem, QApplication
 
-from PyQt6 import QtCore, QtGui, QtWidgets
-
-
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(771, 567)
-        self.tableView = QtWidgets.QTableView(parent=Form)
-        self.tableView.setGeometry(QtCore.QRect(0, 0, 771, 401))
-        self.tableView.setObjectName("tableView")
-
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
-
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+from add_form import Add
+from main_form import Main_Form
 
 
-class Example(QWidget, Ui_Form):
+class Example(QWidget, Main_Form):
     def __init__(self):
+        self.connection = sqlite3.connect("coffee")
         super().__init__()
         self.setupUi(self)
-        # Зададим тип базы данных
-        db = QSqlDatabase.addDatabase('QSQLITE')
-        # Укажем имя базы данных
-        db.setDatabaseName('coffee.sqlite')
-        # И откроем подключение
-        db.open()
+        self.createFields()
+        self.list_modified = []
 
-        # QTableView - виджет для отображения данных из базы
-        # Создадим объект QSqlTableModel,
-        # зададим таблицу, с которой он будет работать,
-        #  и выберем все данные
-        model = QSqlTableModel(self, db)
-        model.setTable('coffee')
-        model.select()
+        res = self.connection.cursor().execute("SELECT * FROM coffee")
+        self.pushButton.clicked.connect(self.add_row)
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        self.data = {}
+        for i, row in enumerate(res):
+            self.tableWidget.insertRow(i)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect((self.save_results))
+        self.modified = {}
 
-        # Для отображения данных на виджете
-        # свяжем его и нашу модель данных
-        self.tableView.setModel(model)
+    def item_changed(self, item):
+        row = item.row()
+        rowdata = {}
+        for fieldId, fieldName in self.fields.items():
+            rowdata[fieldName] = self.tableWidget.item(row, fieldId).text()
+        self.modified[row] = rowdata
 
-        self.setGeometry(300, 100, 650, 450)
-        self.setWindowTitle('Пример работы с QtSql')
+    def save_results(self):
+        if self.modified:
+            cur = self.connection.cursor()
+            for data in self.modified.values():
+                que = "UPDATE coffee\n"
+                que += 'SET'
+                que += '\n'
+                que += ", ".join([f"{key}='{data.get(key)}'"
+                                  for key in data.keys()])
+                que += '\n'
+                que += f"WHERE id = {data.get('id')}"
+            cur.execute(que)
+            self.connection.commit()
+            self.modified.clear()
+
+    def add_row(self):
+        self.addWindows = Add(self.tableWidget)
+        self.addWindows.show()
+
+    def createFields(self):
+        cur = self.connection.cursor()
+        cur.execute('PRAGMA table_info("coffee")')
+        self.fields = {}
+        for i, col in enumerate(cur.fetchall()):
+            self.tableWidget.insertColumn(i)
+            self.fields[i] = col[1]
+
+
 
 
 if __name__ == '__main__':
